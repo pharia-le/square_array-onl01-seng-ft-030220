16d062eae28cb387b14bdd73e61514d6e88e2b36
Pharia Le <pharia.le@gmail.com>
2020-03-16 01:06:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   array.each_with_index { |num, i| array[i] = num*num }
 end
+
+def square_array(array)
+  array.each_with_index do |num, i|
+    array[i] = num*num
+  end
+end
